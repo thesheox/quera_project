2069895a51cba4f39f273fcd7384fb169f59bb24
thesheox <thesheox@gmail.com>
2024-04-25 13:29:29 +0330

    commit massage

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,27 +1,32 @@
 from rest_framework import serializers
 
+from charities.models import Charity, Benefactor
+from .models import User
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+from rest_framework import serializers
+
 from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
-
     class Meta:
-        model = User
-        fields = (
-            'username',
-            'password',
-            'phone',
-            'address',
-            'gender',
-            'age',
-            'description',
-            'first_name',
-            'last_name',
-            'email',
-        )
-
+        model=User
+        fields=('username','password','phone','address','gender','age','description','first_name','last_name','email')
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
-        return user
+        the_user = User.objects.create_user(**validated_data)
+        return the_user
 
+
+class BenefactorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Benefactor
+        fields = ['experience', 'free_time_per_week']
+
+
+class CharitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Charity
+        fields = ['name', 'reg_number']
--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,11 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import generics
+
+from charities.models import Charity, Benefactor
+from .models import User
+from .serializers import UserSerializer, CharitySerializer, BenefactorSerializer
 
 from .serializers import UserSerializer
 
@@ -18,8 +23,24 @@ class LogoutAPIView(APIView):
         )
 
 
+
 class UserRegistration(generics.CreateAPIView):
     permission_classes = (AllowAny,)
     authentication_classes = []
     serializer_class = UserSerializer
 
+
+class BenefactorRegistration(generics.CreateAPIView):
+    queryset = Benefactor.objects.all()
+    serializer_class = BenefactorSerializer
+
+    def perform_create(self, serializer):
+        BenefactorSerializer.save(user=self.request.user)
+
+
+class CharityRegistration(generics.CreateAPIView):
+    queryset = Charity.objects.all()
+    serializer_class = CharitySerializer
+
+    def CharitySerializer(self, serializer):
+        serializer.save(user=self.request.user)
