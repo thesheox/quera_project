f1d309e7534d1dde3040076544ea60412ab26986
thesheox <thesheox@gmail.com>
2024-04-30 12:58:13 +0330

    commit massage

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,18 @@
 from rest_framework import serializers
-from .models import Benefactor, Charity
+from charities.models import Benefactor, Charity
+from rest_framework import serializers
 
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    class Meta:
+        model=User
+        fields=('username','password','phone','address','gender','age','description','first_name','last_name','email')
+    def create(self, validated_data):
+        the_user = User.objects.create_user(**validated_data)
+        return the_user
 class BenefactorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Benefactor
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,26 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from charities.models import Charity, Benefactor
 from .serializers import BenefactorSerializer, CharitySerializer
-from .models import Benefactor, Charity
+from rest_framework import generics
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializers import UserSerializer
+
+class LogoutAPIView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        request.user.auth_token.delete()
+        return Response(
+            data={'message': f'Bye {request.user.username}!'},
+            status=status.HTTP_204_NO_CONTENT
+        )
 
 @api_view(['POST'])
 def BenefactorRegistration(request):
@@ -21,3 +39,37 @@ def CharityRegistration(request):
             serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LogoutAPIView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        request.user.auth_token.delete()
+        return Response(
+            data={'message': f'Bye {request.user.username}!'},
+            status=status.HTTP_204_NO_CONTENT
+        )
+
+
+
+class UserRegistration(generics.CreateAPIView):
+    permission_classes = (AllowAny,)
+    authentication_classes = []
+    serializer_class = UserSerializer
+
+
+class BenefactorRegistration(generics.CreateAPIView):
+    queryset = Benefactor.objects.all()
+    serializer_class = BenefactorSerializer
+
+    def perform_create(self, serializer):
+        BenefactorSerializer.save(user=self.request.user)
+
+
+class CharityRegistration(generics.CreateAPIView):
+    queryset = Charity.objects.all()
+    serializer_class = CharitySerializer
+
+    def CharitySerializer(self, serializer):
+        serializer.save(user=self.request.user)
